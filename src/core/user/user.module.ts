c59c0526a7eb285d793c0bed63ed1850253fe01d
chunshan <1484082125@qq.com>
2022-03-29 02:20:51 +0800

    feta: jsconfig.json

--- src/core/user/user.module.ts
+++ src/core/user/user.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from "./entities/user.entity"
 import { oAuthModule } from 'src/share/oAuth/oAuth.module';
 import { JwtStrategy } from 'src/share/jwt/jwt.strategy';
+
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
