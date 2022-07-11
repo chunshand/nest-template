04e787c2f000faffbed736a0bde96ea860415510
chunshan <1484082125@qq.com>
2022-07-11 09:23:02 +0800

    style(): 🎨 增加了点注释

--- src/core/user/dto/login-user.dto.ts
+++ src/core/user/dto/login-user.dto.ts
@@ -6,3 +6,9 @@ export class LoginUserDto extends OmitType(CreateUserDto, ['type'] as const)
 {
 
 }
+
+// PartialType 生成一个类，并将字段全部设置可选
+// OmitType 移除一些字段
+// PickType 只保留的一些字段
+// IntersectionType 组合两种类型
+
